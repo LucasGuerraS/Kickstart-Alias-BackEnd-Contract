ed10336ea54bc16ead3414b0fb238204e12442ac
LucasGuerraS <lucasgasardo@gmail.com>
2022-10-21 18:11:24 -0300

    🌱 updated addres of deployed contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2af716e82210dE6350Ffc00745b3c29ee35D2538"
+  "0x6232acdEa22AbB0ba7E80747c74a9dFcdb650187"
 );
 
 export default instance;
