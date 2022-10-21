eca5c78d62fcb548294e595ac44520f071226c20
LucasGuerraS <lucasgasardo@gmail.com>
2022-10-21 18:42:02 -0300

    🚧 Working on contribute form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,73 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import createContract from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = createContract(props.query.address);
+
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      approversCount,
+      requestsCount,
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of the manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: {overflowWrap: "break-word"}
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (Wei)",
+        description: "You must contribute at least this much wei to beacome an approver"
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description: "A request tries to withdraw money from the contract. Requests must be approved by more than 50% of the approvers"
+      }, 
+      {
+        header: approversCount,
+        meta: "Number of approvers",
+        description: "Number of people who have contributed to this campaign"
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description: "The balance is how much money this campaign has left to spend"
+      }
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
         <h3>Campaign show</h3>
+        {this.renderCards()}
+        <ContributeForm />
       </Layout>
     );
   }
