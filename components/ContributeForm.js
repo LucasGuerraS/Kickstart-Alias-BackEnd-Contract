37e08e93e8a3fed898948374b3282e776ddcd9b8
LucasGuerraS <lucasgasardo@gmail.com>
2022-10-24 18:02:29 -0300

    ✅ Finalized Project and added updated versions

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,28 +1,52 @@
 import React, { Component } from "react";
 import { Button, Form, Input, Message } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
+import { Router } from "../routes";
 
 class ContributeForm extends Component {
   state = {
     value: "",
+    errorMessage: "",
+    loading: false,
   };
 
-  onSubmit = (e) => {
+  onSubmit = async (e) => {
     e.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether"),
+      });
+
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (error) {
+      this.setState({ errorMessage: error.message });
+    }
+
+    this.setState({ loading: false, value: "" });
   };
-    
+
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
             label="ether"
             labelPosition="right"
             value={this.state.value}
-            onChange={(e) => this.state.value({ value: e.target.value })}
+            onChange={(e) => this.setState({ value: e.target.value })}
           />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button loading={this.state.loading} primary>Contribute!</Button>
       </Form>
     );
   }
--- ethereum/campaign.js
+++ ethereum/campaign.js
@@ -6,3 +6,14 @@ const createContract = (address) => {
 };
 
 export default createContract;
+
+
+//TODO Updated Version
+
+// import web3 from "./web3";
+// import Campaign from "./build/Campaign.json";
+ 
+// const campaign = (address) => {
+//   return new web3.eth.Contract(Campaign.abi, address);
+// };
+// export default campaign;
